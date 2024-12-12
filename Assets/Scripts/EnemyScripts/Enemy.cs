e9509fa1bfe46d13547ae6a5473e111bbfd47b75
Quachcan <144897202+Quachcan@users.noreply.github.com>
2024-12-12 13:28:03 +0700

    First Commit
    Manager:
    - UI, Game, Scene, Animation
    Player:
    - Movement
    Enemy:
    - NavMesh, Spawner, ScriptableObject

--- Assets/Scripts/EnemyScripts/Enemy.cs
+++ Assets/Scripts/EnemyScripts/Enemy.cs
@@ -1,18 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Serialization;
 
-public class Enemy : MonoBehaviour
+namespace EnemyScripts
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Enemy : MonoBehaviour
     {
-        
-    }
+        private int _health;
 
-    // Update is called once per frame
-    void Update()
-    {
+        public float distance;
         
+        public Transform player;
+        private NavMeshAgent agent;
+
+        private void Awake()
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+            agent = GetComponentInChildren<NavMeshAgent>();
+        }
+
+        private void Update()
+        {
+            distance = Vector3.Distance(transform.position, player.transform.position);
+            agent.SetDestination(player.position);
+        }
+
+        public void TakeDamage(int damage)
+        {
+            _health -= damage;
+            Debug.Log($"{name} took {damage} damage, remaining health: {_health}");
+
+            if (_health <= 0)
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {
+            Debug.Log($"{name} died.");
+            Destroy(gameObject); // Xóa Enemy khi chết
+        }
     }
 }
